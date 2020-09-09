e7858e9dfb25e0a8a3ad8b9cbae202caacd676e9
Joshua Singleton <jsingle345@gmail.com>
2020-09-09 05:29:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,19 @@ end
 
 
 def consolidate_cart(cart)
- 
+  count = 1
+  cart_combined = []
+  cart.each do |item_info|
+    item_info[:count] = 1
+    if !cart_combined.include? item_info
+      cart_combined.push(item_info)
+    else
+      for i in cart_combined
+        i[:count] += 1
+      end
+    end
+  end
+  return cart_combined
 end
  # Consult README for inputs and outputs
   #
