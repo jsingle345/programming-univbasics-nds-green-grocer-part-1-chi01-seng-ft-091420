9170e225f71c2682b97765de2c03c7ca27344e07
Joshua Singleton <jsingle345@gmail.com>
2020-09-09 05:35:04 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,38 @@
+require "pry"
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  
+  collection.each do |element|
+    if element[:item] == name
+      return element
+    end 
+  end
+ nil
 end
 
+
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
+  cart_combined = []
+  cart.each do |item_info|
+    item_info[:count] = 1
+    food_name = find_item_by_name_in_collection(item_info[:item], cart_combined)
+    if !food_name 
+      cart_combined.push(item_info)
+    else
+      for i in cart_combined
+        if i[:item] == item_info[:item]
+          i[:count] += 1
+        end
+      end
+    end
+    end
+  return cart_combined
+end
+
+
+ # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-end
 
-
-  
+#binding.pry
